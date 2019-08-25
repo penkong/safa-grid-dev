34eddcc91cbade2e8af21649638a4d9325928820
mk-nazemi <nazemi.works@gmail.com>
2019-08-25 14:31:02 +0430

    make some changes for build and dev packages.;

---
+++ src/components/Label/SLabel.vue
@@ -0,0 +1,24 @@
+<template>
+  <label :for="rowLabel">
+    <slot>{{rowLabel}}</slot>
+  </label>
+</template>
+
+<script>
+export default {
+  name: "SLabel",
+  props: {
+    rowLabel: {
+      type: String,
+      default: "",
+      required: true,
+      validator: function() {
+        return;
+      }
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
